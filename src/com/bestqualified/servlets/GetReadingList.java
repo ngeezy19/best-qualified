cd2e79088739ee7549b2428d5d6e3e49092afd47
FrankDupree <frankyotana@yahoo.com>
2016-08-08 11:50:40 +0100

    updates

--- src/com/bestqualified/servlets/GetReadingList.java
+++ src/com/bestqualified/servlets/GetReadingList.java
@@ -36,9 +36,9 @@ import com.google.appengine.api.urlfetch.URLFetchServiceFactory;
 
 public class GetReadingList extends HttpServlet {
 
-	private static final String AWS_ACCESS_KEY_ID = "AKIAJWQ3Z2P6ALOAAHDQ";
-	private static final String AWS_SECRET_KEY = "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";
-	private static final String ENDPOINT = "webservices.amazon.com";
+	private static final String ak = "";
+	private static final String ask = "";
+	private static final String ep = "";
 
 	private static final long serialVersionUID = -1477727040955965118L;
 
@@ -81,8 +81,8 @@ public class GetReadingList extends HttpServlet {
 		SignedRequestsHelper helper;
 
 		try {
-			helper = SignedRequestsHelper.getInstance(ENDPOINT,
-					AWS_ACCESS_KEY_ID, AWS_SECRET_KEY);
+			helper = SignedRequestsHelper.getInstance(ep,
+					ak, ask);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return;
